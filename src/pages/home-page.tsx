e725e2a8c490d9d4b6e56093a7c4d57b222b0259
Mohit Singh <msxcodes@msxcodes.kali>
2024-06-25 21:26:55 +0530

    fix the build issues

--- src/pages/home-page.tsx
+++ src/pages/home-page.tsx
@@ -1,8 +1,8 @@
 import React from "react";
-import Hero from "./components/hero-section/hero-section";
-import Skills from "./components/skills/skills-section";
-import ContactUs from "./components/contactus/contact-section";
-import Projects from "./components/projects/project-section";
+import Hero from "@/components/hero-section/hero-section";
+import Skills from "@/components/skills/skills-section";
+import ContactUs from "@/components/contactus/contact-section";
+import Projects from "@/components/projects/project-section";
 
 export default function HomePage() {
   return (
